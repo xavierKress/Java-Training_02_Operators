b1b63ae69ed4494fae5b30e408801744b1957b85
xavier <xavier.kress@gmail.com>
2021-03-07 19:38:40 +0100

    add variable in our project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Java-Training_02_Variables
+
+In this tutorial, we will add variables in our project
+
+## Steps
+
+1. Create a new variable of type Integer in our main method
+2. Print it on the console
+  
+
---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+
+    public static void main (String[] args) {
+        int peopleInTheRoom = 5;
+
+        System.out.println("Hello, World!");
+        System.out.println("We are "+peopleInTheRoom+" in this room");
+
+    }
+
+}
